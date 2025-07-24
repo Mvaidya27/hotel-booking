7201d5387ce0d51ba137e045d9d29865c7503c39
Mayur Vaidya <113794955+Mvaidya27@users.noreply.github.com>
2025-07-24 14:51:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 // Api to listen clerk webhooks
-app.use("/api/clerk", clerkWebhooks)
+app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res) => res.send("API is working"))
 
