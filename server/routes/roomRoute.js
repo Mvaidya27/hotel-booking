6d663defbfd2c44bfedb8392c3fca0d9dfae1be7
Mvaidya27 <vaidyamayur.272@gmail.com>
2025-07-27 16:39:01 +0530

    update

---
+++ server/routes/roomRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
+import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from "../controllers/roomController";
+
+const roomRouter = express.Router();
+
+roomRouter.post('/', upload.array("images", 4), protect, createRoom)
+roomRouter.get('/', getRooms)
+roomRouter.get('/owner', protect, getOwnerRooms)
+roomRouter.post('/toggle-availability', protect, toggleRoomAvailability)
+
+export default roomRouter;
